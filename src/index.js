1ba0e5c9e59d324d769f571e7a57c03709430a68
Gianmarco Venturini <gianmarco.venturini@outlook.it>
2020-03-27 23:47:12 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 "use strict";
+const axios = require('axios');
 
+class Service {
+    async getData (url) {
+        try {
+            let response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
 
 module.exports = requestMultipleUrls;
-function requestMultipleUrls(urls) {
-    return 'Test Result'
+async function requestMultipleUrls(url) {
+    try {
+        let service = new Service();
+        let result = await service.getData(url)
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,27 @@
+const axios = require('axios');
+var MockAdapter = require("axios-mock-adapter");
+var mock = new MockAdapter(axios);
+
 const assert = require('assert');
 const requestMultipleUrls = require('../src/index');
 
-describe('index.js tests', function () {
-    describe('main function tests', function () {
-        it('should be a string', function () {
-            const result = requestMultipleUrls([]);
-            assert.equal(result, 'Test Result')
-        });
+const url = 'https://ft-tech-test-example.dummy.com/ftse-fsi.json';
+const jsonData = {
+    'someData': 'someValue'
+}
+
+describe('index.js tests', () => {
+    it('should returned data from mocked external service', async () => {
+        mock.onGet(url).reply(200, jsonData);
+
+        var result = requestMultipleUrls(url);
+
+        result.then((data) => {
+            assert.equal(data.someData, 'someValue');
+            done();
+        }, (error) => {
+            assert.fail(error);
+            done();
+        })
     });
 });
