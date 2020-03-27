4deacd780d849f1a0ed6c5a2b01ac2b430104b39
Gianmarco Venturini <gianmarco.venturini@outlook.it>
2020-03-27 18:40:35 +0100

    project init

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const config = require('./package.json');
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: path.resolve(__dirname, config.main),
+    devtool: 'source-map',
+    output: {
+        library: 'request-multiple-urls',
+        libraryTarget: 'var',
+        path: __dirname,
+        filename: 'build/request-multiple-urls.js' // check for prod env later
+    },
+    module: {
+        loaders: [
+          {test: /\.es6?$/, exclude: /node_modules/, loader: 'babel-loader'}
+        ]
+    },
+    plugins: []
+};
